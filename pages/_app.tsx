60a040f9d628702e938c372eef8b0a5d00fabb08
evantianx <evantianx@gmail.com>
2022-04-03 16:22:38 -0600

    components: PlayerLayout init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,20 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import 'reset-css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import "reset-css"
+import { PlayerLayout } from "../components/playerLayout"
 
 const theme = extendTheme({
   colors: {
     gray: {
-      100: '#f5f5f5',
-      200: '#eeeeee',
-      300: '#e0e0e0',
-      400: '#bdbdbd',
-      500: '#9e9e9e',
-      600: '#757575',
-      700: '#616161',
-      800: '#424242',
-      900: '#212121'
-    }
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
   },
   components: {
     Button: {
@@ -21,18 +22,20 @@ const theme = extendTheme({
         link: {
           ":focus": {
             outline: "none",
-            boxShadow: "none"
-          }
-        }
-      }
-    }
-  }
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
 })
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
     </ChakraProvider>
   )
 }
